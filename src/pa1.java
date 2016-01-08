49ff4ef9d99f3a0c8803680c73b08a319c65fccc
Rick <rick.ramirez6174@gmail.com>
2016-01-07 20:59:48 -0800

    Added a description.

--- src/pa1.java
+++ src/pa1.java
@@ -3,7 +3,7 @@
  * Rick Ramirez
  * riryrami
  * Lawn
- * DESCRIPTION
+ * This program asks the user for input from the keyboard, then performs a calculation and prints the results.
  */
 
 import java.util.Scanner; // import class ... Scanner is in the java.util package
@@ -22,21 +22,24 @@ public class pa1 {
         System.out.print("Enter the length and width of the house, in feet: ");
         double houseLength = input.nextDouble();
         double houseWidth = input.nextDouble();
-        double area = lotLength * lotWidth - houseLength * houseWidth;
 
+        // Calculate and print the area of the lawn.
+        double area = lotLength * lotWidth - houseLength * houseWidth;
         System.out.println("The lawn area is " + area + " square feet.");
 
         // Prompt user for rate at which lawn is mowed.
         System.out.print("Enter the mowing rate, in square feet per second: ");
         double mowRate = input.nextDouble();
 
+        // Determine the hours, minutes and seconds required to mow the lawn.
         int seconds = (int)Math.round(area / mowRate);
         int minutes = seconds / 60;
         int hours = minutes / 60;
         minutes = minutes % 60;
         seconds = seconds % 60;
 
-        System.out.println("The mowing time is " + hours + " hour" +(hours==1? " ":"s ") + minutes
+        // The output is arranged such that plural and singular time components are printed accordingly.
+        System.out.println("The mowing time is " + hours + " hour" + (hours==1? " ":"s ") + minutes
                 + " minute" + (minutes==1? " ":"s ") + seconds + " second" + (seconds==1? "":"s")+".");
 
     }
