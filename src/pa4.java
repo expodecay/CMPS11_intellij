7b22b05bf373f9cf0a5bbd0f6c8a178594ac72a1
Rick <rick.ramirez6174@gmail.com>
2016-02-12 13:02:49 -0800

    pa4 finished

--- src/pa4.java
+++ src/pa4.java
@@ -7,12 +7,13 @@ public class pa4 {
     public static void main(String[] args) {
         // Tolerance = epsilon
         // resolution = width of sub interval
-        // threshold => upper limit for F(evenRoot) = 0 (needed to compensate rounding errors.)
+        // threshold => upper limit for F(evenRoot) = 0
         double resolution = Math.pow(10,-2), tolerance = Math.pow(10,-7), threshold = Math.pow(10,-3);
         int degree;
         double[] c;
         double left, right, a, b;
 
+        // Prompt user for degree of polynomial.
         Scanner input = new Scanner(System.in);
         System.out.print("Enter the degree: ");
         degree = input.nextInt();
@@ -23,28 +24,39 @@ public class pa4 {
         for(int i =0; i < degree+1; i++){
             c[i] = input.nextDouble();
         }
+
+        // Prompt user for endpoints
         System.out.print("Enter the left and right endpoints: ");
         left = input.nextDouble();
         right = input.nextDouble();
-/*********************************************************/
-        // Main loop
+
+        /*  Main Loop:   Same logic as findRoot() */
         a = left;
+        int i = 0; // Keep track of number of zeros found.
         b = a + resolution;
+
+        // Loop over sub intervals.
         while(a < right){
-            if(poly(c,a) * poly(c,b) <= 0) {
+            if(poly(c,a) * poly(c,b) <= 0) {  // If function changes sign in sub-interval, find root.
                 double root = findRoot(c, a, b, tolerance);
-                System.out.printf("%.5f%n", root);
+                System.out.print("Root found at ");
+                System.out.printf("%.5f%n", root);  // Print root to within 5 decimal places.
+                i++;
             }
-            else if(poly(diff(c),a) * poly(diff(c), b) <= 0){
+            else if(poly(diff(c),a) * poly(diff(c), b) <= 0){  // If F'(x) changes sign, find even root.
                 double root = findRoot(diff(c), a, b, tolerance);
-                if(Math.abs(poly(c,root)) < threshold){
+                if(Math.abs(poly(c,root)) < threshold){  // Make sure even root is zero in F(x).
+                    System.out.print("Root found at ");
                     System.out.printf("%.5f%n",root);
+                    i++;
                 }
             }
-            a = b;
-            b = a + resolution;
+            a = b;                  // Shift to next sub interval
+            b = a + resolution;     //
         }
-
+        if(i == 0) {
+            System.out.println("No roots were found in the specified range");
+        }
     }
     // Return the value of F(x)
     //c_0 + c_1 x^1 + c_2 x^2 + ... + c_n x^n
@@ -56,6 +68,7 @@ public class pa4 {
         return sum;
     }
 
+    // Calculate derivative array.
     static double[] diff(double[] c){
         double[] d = new double[c.length-1];
         for(int i = 0; i<d.length; i++){
@@ -64,18 +77,19 @@ public class pa4 {
         return d;
     }
 
+    // Zero finder
     static double findRoot(double[] C, double a, double b, double tolerance){
-        double root = 0.0 , residual;
-        while ( b - a > tolerance ) {
-            root = (a + b) / 2.0;
-            residual = poly(C, root);
-            if (residual > 0)
-                b = root; // replace right endpoint
-            else
-                a = root; // replace left endpoint
-
+        double left=0, right=0, root =0;
+        left = a;
+        right = left + tolerance;
+        while(left < b) {
+            if (poly(C, left) * poly(C, right) < 0) {
+                root = (right + left) / 2;  // If F(x) changes sign, take midpoint of interval as root.
+            }
+            left = right;
+            right = left + tolerance;
         }
         return root;
-
     }
+
 }
