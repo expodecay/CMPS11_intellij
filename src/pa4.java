d68153d77baa94958b164e31de03350f146f29d9
Rick <rick.ramirez6174@gmail.com>
2016-02-12 10:41:57 -0800

    Need to fix rounding errors in pa4

--- src/pa4.java
+++ src/pa4.java
@@ -1,17 +1,17 @@
 /**
  * Created by Rick on 2/10/2016.
  */
-import java.util.Arrays;
+
 import java.util.Scanner;
 public class pa4 {
     public static void main(String[] args) {
         // Tolerance = epsilon
         // resolution = width of sub interval
         // threshold => upper limit for F(evenRoot) = 0 (needed to compensate rounding errors.)
-        double resolution = 10*Math.pow(10,-2), tolerance = 10*Math.pow(10,-7), threshold = 10*Math.pow(10,-3);
-        int degree = 0;
+        double resolution = Math.pow(10,-2), tolerance = Math.pow(10,-7), threshold = Math.pow(10,-3);
+        int degree;
         double[] c;
-        double left, right;
+        double left, right, a, b;
 
         Scanner input = new Scanner(System.in);
         System.out.print("Enter the degree: ");
@@ -20,50 +20,62 @@ public class pa4 {
 
         // Populate coefficient matrix
         System.out.print("Enter " + (degree + 1) + " coefficients: ");
-        for(int i =0; i<degree+1; i++){
+        for(int i =0; i < degree+1; i++){
             c[i] = input.nextDouble();
         }
-        // Populate derivative coefficient matrix
-        double[] derivative = diff(c);
         System.out.print("Enter the left and right endpoints: ");
         left = input.nextDouble();
         right = input.nextDouble();
-        resolution = left   - right;
-        System.out.println(degree);
+/*********************************************************/
+        // Main loop
+        a = left;
+        b = a + resolution;
+        while(a < right){
+            if(poly(c,a) * poly(c,b) <= 0) {
+                double root = findRoot(c, a, b, tolerance);
+                System.out.printf("%.5f%n", root);
+            }
+            else if(poly(diff(c),a) * poly(diff(c), b) <= 0){
+                double root = findRoot(diff(c), a, b, tolerance);
+                if(Math.abs(poly(c,root)) < threshold){
+                    System.out.printf("%.5f%n",root);
+                }
+            }
+            a = b;
+            b = a + resolution;
+        }
 
-        System.out.println(Arrays.toString(c));
-        System.out.println(left + "   " + right);
-
-        double value = 2;
-        double ass = poly(c,value);
-
-
-        System.out.println("value at f(a): " + ass);
-        System.out.println("Deriv of coefficients: " + Arrays.toString(derivative));
-        System.out.println(Arrays.toString(c));
     }
     // Return the value of F(x)
     //c_0 + c_1 x^1 + c_2 x^2 + ... + c_n x^n
     static double poly(double[] c, double x){
-        double sum = 0;
-        for(int i=0 ; i<x + 1; i++){
-            c[i] *= Math.pow(x,i);
-            sum += c[i];
+        double sum = 0.0;
+        for(int i=0 ; i < c.length ; i++){
+            sum += c[i] * Math.pow(x,i);
         }
         return sum;
     }
 
     static double[] diff(double[] c){
         double[] d = new double[c.length-1];
-
         for(int i = 0; i<d.length; i++){
-            d[i] = c[i];
+            d[i] = (i+1) * c[i+1];
         }
         return d;
     }
-/*
+
     static double findRoot(double[] C, double a, double b, double tolerance){
+        double root = 0.0 , residual;
+        while ( b - a > tolerance ) {
+            root = (a + b) / 2.0;
+            residual = poly(C, root);
+            if (residual > 0)
+                b = root; // replace right endpoint
+            else
+                a = root; // replace left endpoint
+
+        }
+        return root;
 
     }
-    */
 }
